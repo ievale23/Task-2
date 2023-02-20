8601b412dff8e76504af59bf5c304bca5113c929
ievale23 <ieva.lebedyte@gmail.com>
2023-02-20 05:50:57 +0200

    galim paspausti jau

---
+++ script.js
@@ -0,0 +1,10 @@
+/* ------------------------------ TASK 2 ----------------------------
+Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
+su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
+pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
+------------------------------------------------------------------- */
+function spaudziam() {
+  console.log("paspaudėm");
+}
+
+document.getElementById("btn__element").addEventListener("click", spaudziam);
